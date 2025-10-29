42379d20d6575927ebdae52c62b2c606d7220ff8
AKASH A <akashpersonal.a@gmail.com>
2025-10-29 13:46:09 +0530

    update

---
+++ src/assets/services_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Protfolio",
+        s_desc:"Web development is the process of building, programming...",
+        s_link:""
+    },
+    {
+        s_no:"02",
+        s_name:"FoodApp",
+        s_desc:"Web development is the process of building, programming...",
+        s_link:"https://food-del-fe.vercel.app/"
+    },
+    {
+        s_no:"03",
+        s_name:"Social media App",
+        s_desc:"Web development is the process of building, programming...",
+        s_link:"https://social-app-olive-nu.vercel.app/"
+    },
+    {
+        s_no:"04",
+        s_name:"Farmdash",
+        s_desc:"Web development is the process of building, programming...",
+        s_link:"https://finalyear-ashy.vercel.app/"
+    },
+    {
+        s_no:"05",
+        s_name:"Gemini",
+        s_desc:"Web development is the process of building, programming...",
+        s_link:"https://gemini-nine-vert.vercel.app/"
+    }
+]
+ 
+export default Services_Data;
